eeb0b412e0bc16ed7b295a1db4718b9a3e8a5fbb
rombru <rom.bruyere@gmail.com>
2024-02-29 22:05:03 +0100

    fix: get geometry type by reading the type attribute

--- core/get_geometry_type.py
+++ core/get_geometry_type.py
@@ -5,4 +5,4 @@ def get_geometry_type(data: list[object], geometry_field: str):
     if len(data) == 0:
         return GeometryType.POINT
     else:
-        GeometryType.from_geojson_type(data[0][geometry_field].type)
+        return GeometryType.from_geojson_type(data[0][geometry_field]["type"])
