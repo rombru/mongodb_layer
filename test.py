7679b4200c9374995f8e188b113c836d9cce9118
rombru <rom.bruyere@gmail.com>
2024-03-18 18:53:24 +0100

    fix: add async event loop for testing

--- test.py
+++ test.py
@@ -3,14 +3,16 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QMainWindow)
 
+from core import async_utils
 from core.mongodb_layer_dockwidget import MongoDBLayerDockWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.loop = async_utils.create_loop()
         self.resize(400, 600)
-        self.addDockWidget(Qt.RightDockWidgetArea, MongoDBLayerDockWidget())
+        self.addDockWidget(Qt.RightDockWidgetArea, MongoDBLayerDockWidget(self.loop))
 
 
 if __name__ == '__main__':
