0ef6ace86a8076c1b6e2b68629706e4ae5ce2df8
rombru <rom.bruyere@gmail.com>
2024-03-19 18:52:26 +0100

    feat: remove WKT format for now

--- core/enums/geometry_format.py
+++ core/enums/geometry_format.py
@@ -3,13 +3,13 @@ from enum import Enum
 
 class GeometryFormat(Enum):
     GEOJSON = "GeoJSON"
-    WKT = "WKT"
+    # WKT = "WKT"
 
     @staticmethod
     def from_str(label):
         if label == GeometryFormat.GEOJSON.value:
             return GeometryFormat.GEOJSON
-        elif label == GeometryFormat.WKT.value:
-            return GeometryFormat.WKT
+        # elif label == GeometryFormat.WKT.value:
+        #     return GeometryFormat.WKT
         else:
             raise NotImplementedError
